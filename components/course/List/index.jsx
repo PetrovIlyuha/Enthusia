4f0a6cedc24dc7cd9683f93b387def6972e376f8
Petrov Ilya <dnamix1@gmail.com>
2021-11-17 14:11:20 +0300

    made necessary changes for data representaion in product-list and single product page hero component

--- components/course/List/index.jsx
+++ components/course/List/index.jsx
@@ -1,33 +1,37 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
-const CourseList = () => {
+const CourseList = ({ products }) => {
   return (
-    // <section className='grid grid-cols-2 gap-4 mb-5'>
-    <section class='container px-6 py-4 mx-auto'>
-      <div class='grid gap-6 mb-8 md:grid-cols-1 lg:grid-cols-2'>
-        {Array.from({ length: 4 }).map((_, i) => (
-          <Link href='/course' key={i}>
-            <div class='flex items-center cursor-pointer group p-4 transform transition-all bg-white border-2 border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 hover:-translate-y-1 hover:shadow-lg hover:text-white hover:bg-blue-600'>
-              <img
-                alt='mountain'
-                class='w-45 rounded-md border-2 border-gray-300'
-                src='https://picsum.photos/seed/picsum/200'
-              />
-              <div id='body' class='flex flex-col ml-5'>
-                <h4 id='name' class='text-xl font-semibold mb-2'>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit
+    <section className='container px-6 py-4 mx-auto'>
+      <div className='grid gap-6 mb-8 md:grid-cols-1 lg:grid-cols-2'>
+        {products?.map(product => (
+          <Link href={`/products/${product.id}`} key={product.id}>
+            <div className='flex items-center cursor-pointer group p-4 transform transition-all bg-white border-2 border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 hover:-translate-y-1 hover:shadow-lg hover:text-white hover:bg-blue-600'>
+              <div className='relative w-full h-full rounded-md border-2 border-gray-300'>
+                <Image
+                  alt={product.title}
+                  layout='fill'
+                  objectFit='cover'
+                  src={product.coverImage}
+                />
+              </div>
+              <div id='body' className='flex flex-col ml-5'>
+                <h4 id='name' className='text-xl font-semibold mb-2'>
+                  {product.title}
                 </h4>
-                <p id='job' class='text-gray-800 group-hover:text-white  mt-2'>
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat.
+                <p
+                  id='job'
+                  className='text-gray-800 group-hover:text-white  mt-2'>
+                  {product.description}
                 </p>
-                <div class='flex mt-5'>
+                <div className='flex mt-5'>
                   <img
                     alt='avatar'
-                    class='w-6 rounded-full border-2 border-gray-300'
+                    className='w-6 rounded-full border-2 border-gray-300'
                     src='https://picsum.photos/seed/picsum/200'
                   />
-                  <p class='ml-3'>John Doe</p>
+                  <p className='ml-3'>{product.author}</p>
                 </div>
               </div>
             </div>
--- components/course/Single/Hero/index.jsx
+++ components/course/Single/Hero/index.jsx
@@ -1,4 +1,12 @@
-export default function CourseHero() {
+export default function CourseHero({ title, description, coverImage }) {
+  const titleLengthInWords = title.split(' ').length;
+  const titleWords = title.split(' ');
+  const wordsInWhite = titleWords
+    .slice(0, Math.ceil(titleLengthInWords / 2))
+    .join(' ');
+  const wordsInYellow = titleWords
+    .slice(Math.ceil(titleLengthInWords / 2))
+    .join(' ');
   return (
     <section>
       <div className='relative'>
@@ -7,21 +15,19 @@ export default function CourseHero() {
           <div className='relative shadow-xl sm:rounded-2xl sm:overflow-hidden'>
             <div className='absolute inset-0'>
               <img
-                className='h-full w-full object-cover'
-                src='https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100'
+                className='h-full w-full object-cover animate-changeScale duration-300'
+                src={coverImage}
                 alt='People working on laptops'
               />
               <div className='absolute inset-0 bg-indigo-700 mix-blend-multiply' />
             </div>
             <div className='relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8'>
               <h1 className='text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl'>
-                <span className='block text-white'>Take control of your</span>
-                <span className='block text-indigo-200'>customer support</span>
+                <span className='block text-white'>{wordsInWhite}</span>
+                <span className='block text-yellow-200'>{wordsInYellow}</span>
               </h1>
               <p className='mt-6 max-w-lg mx-auto text-center text-xl text-indigo-200 sm:max-w-3xl'>
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-                lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-                fugiat aliqua.
+                {description}
               </p>
               <div className='mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center'>
                 <div className='space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5'>
