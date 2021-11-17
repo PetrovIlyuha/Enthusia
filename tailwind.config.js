ade807242ab2471f260fda770a2883867e002224
Petrov Ilya <dnamix1@gmail.com>
2021-11-17 14:08:02 +0300

    added domain for cdn-hosted images in next.config.js and created custom animation for tailwind usage

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        changeScale: {
+          '0%, 100%': { transform: 'scale(1.1)' },
+          '50%': { transform: 'scale(1)' },
+        },
+      },
+      animation: {
+        changeScale: 'changeScale 15s linear infinite forwards',
+      },
+    },
   },
   variants: {
     extend: {},
