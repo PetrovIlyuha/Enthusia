77eb5760dcdb9f3e780f3c2e56b0408125a0e973
Petrov Ilya <dnamix1@gmail.com>
2021-11-17 14:12:24 +0300

    added dynamically-generated page pre-rendered with getStaticProps & getStaticPaths for products

---
+++ pages/products/[id].jsx
@@ -0,0 +1,36 @@
+import Modal from '@components/common/Modal';
+import CourseHero from '@components/course/Single/Hero/index';
+import KeyPoints from '@components/course/Single/KeyPoints/index';
+import Lectures from '@components/course/Single/Lectures/index';
+import { getAllCourses } from 'content/courses/getCourses';
+
+export default function Course({ product }) {
+  return (
+    <div className='fit-page-shell'>
+      <div className='relative max-w-7xl mx-auto px-4'>
+        <CourseHero
+          title={product.title}
+          description={product.description}
+          coverImage={product.coverImage}
+        />
+        <KeyPoints features={product.wsl} />
+        <Lectures />
+        <Modal />
+      </div>
+    </div>
+  );
+}
+
+export function getStaticPaths() {
+  const { data } = getAllCourses();
+  const paths = data.map(product => ({
+    params: { id: product.id },
+  }));
+  return { paths, fallback: 'blocking' };
+}
+
+export function getStaticProps({ params }) {
+  const { courseMap } = getAllCourses();
+
+  return { props: { product: courseMap[params.id] } };
+}
