43392dc743122ee7359a1999392c7d3ff190a5c0
Petrov Ilya <dnamix1@gmail.com>
2021-12-02 13:39:52 +0300

    refactored web3 prodiver to effectively reduce number of times that app hooks will get instantiated

--- components/providers/web3/index.jsx
+++ components/providers/web3/index.jsx
@@ -1,58 +1,71 @@
 import detectEthereumProvider from '@metamask/detect-provider';
-import { createContext, useContext, useEffect, useMemo, useState } from 'react';
+import {createContext, useContext, useEffect, useMemo, useState} from 'react';
 import Web3 from 'web3';
 import setupWeb3Hooks from './hooks/setupWeb3Hooks';
 
 const Web3Context = createContext(null);
 
-const Web3Provider = ({ children }) => {
-  const [web3Api, setWeb3Api] = useState({
-    provider: null,
-    web3: null,
-    contract: null,
-    isInitialized: false,
-  });
+const Web3Provider = ({children}) => {
+    const [web3Api, setWeb3Api] = useState({
+        provider: null,
+        web3: null,
+        contract: null,
+        isInitialized: false,
+        hooks: setupWeb3Hooks()
+    });
 
-  useEffect(() => {
-    const loadProvider = async () => {
-      const provider = await detectEthereumProvider();
-      if (provider) {
-        const web3 = new Web3(provider);
-        setWeb3Api({ provider, web3, contract: null, isInitialized: true });
-      } else {
-        setWeb3Api(api => ({ ...api, isInitialized: true }));
-        console.error('You need to install Metamask Chrome Extension.');
-      }
-    };
-    loadProvider();
-  }, []);
-
-  const _web3Api = useMemo(() => {
-    return {
-      ...web3Api,
-      loading: !web3Api.isInitialized,
-      hooks: setupWeb3Hooks(web3Api.web3),
-      metamaskIsReady: web3Api.isInitialized && web3Api.web3,
-      connect: web3Api.provider
-        ? async () => {
-            try {
-              await web3Api.provider.request({ method: 'eth_requestAccounts' });
-            } catch (error) {
-              location.reload();
+    useEffect(() => {
+        const loadProvider = async () => {
+            const provider = await detectEthereumProvider();
+            if (provider) {
+                const web3 = new Web3(provider);
+                setWeb3Api({
+                    provider,
+                    web3,
+                    contract: null,
+                    isInitialized: true,
+                    hooks: setupWeb3Hooks(web3, provider)
+                });
+            } else {
+                setWeb3Api(api => ({...api, isInitialized: true}));
+                console.error('You need to install Metamask Chrome Extension.');
             }
-          }
-        : () => {
-            console.error("Can't establish connection to MetaMask.");
-          },
-    };
-  }, [web3Api]);
-  return (
-    <Web3Context.Provider value={_web3Api}>{children}</Web3Context.Provider>
-  );
+        };
+        loadProvider();
+    }, []);
+
+    const _web3Api = useMemo(() => {
+        const {provider, web3, isInitialized} = web3Api;
+        return {
+            ...web3Api,
+            loading: !web3Api.isInitialized,
+            metamaskIsReady: isInitialized && web3,
+            connect: provider
+                ? async () => {
+                    console.log("connection with MetaMask")
+                    try {
+                        await provider.request({method: 'eth_requestAccounts'});
+                    } catch (error) {
+                        location.reload();
+                    }
+                }
+                : () => {
+                    console.error("Can't establish connection to MetaMask.");
+                },
+        };
+    }, [web3Api]);
+    return (
+        <Web3Context.Provider value={_web3Api}>{children}</Web3Context.Provider>
+    );
 };
 
 export const useWeb3Context = () => {
-  return useContext(Web3Context);
+    return useContext(Web3Context);
 };
+
+export function useHooks(cb) {
+    const {hooks} = useWeb3Context();
+    return cb(hooks)
+}
 
 export default Web3Provider;
--- components/ui/common/ConnectButton.jsx
+++ components/ui/common/ConnectButton.jsx
@@ -1,37 +1,46 @@
-import { motion } from 'framer-motion';
+import {motion} from 'framer-motion';
 
-import { useWeb3Context } from '@components/providers/web3';
+import {useWeb3Context} from '@components/providers/web3';
+import {useAccount} from "@components/hooks/web3";
 
 const ConnectButton = () => {
-  const { metamaskIsReady, loading, connect } = useWeb3Context();
-  return loading ? (
-    <motion.span
-      initial={{ x: -100, opacity: 0 }}
-      animate={{ x: 0, opacity: 1, transition: { delay: 0.08, duration: 0.1 } }}
-      onClick={connect}
-      disabled={true}
-      className={`disabled:cursor-not-allowed ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gray-300`}>
-      loading...
-    </motion.span>
+  const {metamaskIsReady, loading, connect} = useWeb3Context();
+  const {account} = useAccount();
+  return account.data ? (
+      <motion.span
+          initial={{x: -100, opacity: 0}}
+          animate={{x: 0, opacity: 1, transition: {delay: 0.08, duration: 0.1}}}
+          className={`cursor-default ml-8 inline-flex items-center justify-center px-4 py-2 border-transparent rounded-md shadow-sm text-base font-medium text-gray-500 bg-gray-50`}>
+        {account.isAdmin ? "Hello, Admin." : "Welcome to Enthusia"}
+      </motion.span>
+  ) : loading ? (
+      <motion.button
+          initial={{x: -100, opacity: 0}}
+          animate={{x: 0, opacity: 1, transition: {delay: 0.08, duration: 0.1}}}
+          onClick={connect}
+          disabled={true}
+          className={`disabled:cursor-not-allowed ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gray-300`}>
+        loading...
+      </motion.button>
   ) : metamaskIsReady ? (
-    <motion.button
-      initial={{ x: -100, opacity: 0 }}
-      animate={{ x: 0, opacity: 1, transition: { delay: 0.08, duration: 0.1 } }}
-      onClick={connect}
-      disabled={loading}
-      className={`disabled:opacity-50 disabled:cursor-not-allowed ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700`}>
-      Connect Wallet
-    </motion.button>
+      <motion.button
+          initial={{x: -100, opacity: 0}}
+          animate={{x: 0, opacity: 1, transition: {delay: 0.08, duration: 0.1}}}
+          onClick={connect}
+          disabled={loading}
+          className={`disabled:opacity-50 disabled:cursor-not-allowed ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700`}>
+        Connect Wallet
+      </motion.button>
   ) : (
-    <motion.a
-      initial={{ x: -100, opacity: 0 }}
-      animate={{ x: 0, opacity: 1, transition: { delay: 0.08, duration: 0.1 } }}
-      href='https://metamask.io/download'
-      rel='noopener noreferer'
-      target='_blank'
-      className='ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700'>
-      Install MetaMask
-    </motion.a>
+      <motion.a
+          initial={{x: -100, opacity: 0}}
+          animate={{x: 0, opacity: 1, transition: {delay: 0.08, duration: 0.1}}}
+          href='https://metamask.io/download'
+          rel='noopener noreferer'
+          target='_blank'
+          className='ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700'>
+        Install MetaMask
+      </motion.a>
   );
 };
 
