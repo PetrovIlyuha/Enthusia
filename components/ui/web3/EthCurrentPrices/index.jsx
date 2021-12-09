356293480c0a0a3b22eeb4c2fdaf8cc985699b6e
Petrov Ilya <dnamix1@gmail.com>
2021-12-09 20:51:42 +0300

    implementation of change in ethereum price detection with ui style changing to draw user's attention

--- components/ui/web3/EthCurrentPrices/index.jsx
+++ components/ui/web3/EthCurrentPrices/index.jsx
@@ -1,25 +1,46 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {useEthPrice} from "@components/hooks/useEthPrice";
 
 const EthCurrentPrices = () => {
+  const {data: ethCurrentPrice} = useEthPrice();
+  const [lastPrice, setLastPrice] = useState(null)
+  const [changeInPriceDetected, setChangeInPriceDetected] = useState(false);
+
+  useEffect(() => {
+    if (ethCurrentPrice !== lastPrice) {
+      setLastPrice(ethCurrentPrice)
+      setChangeInPriceDetected(true)
+      setTimeout(() => {
+        setChangeInPriceDetected(false);
+      }, 600)
+    } else {
+      setLastPrice(ethCurrentPrice);
+    }
+  }, [ethCurrentPrice])
+
+  console.log({lastPrice, changeInPriceDetected})
   return (
-    <div className='grid grid-cols-4 my-5'>
-      <div className='flex flex-1 items-stretch text-center'>
-        <div className='p-10 border drop-shadow rounded-md'>
-          <div>
-            <span className='text-2xl font-bold'>ETH = 3145.1$</span>
+      <div className='max-w-3xl mx-auto flex justify-center'>
+        <div className='grid grid-cols-2 space-x-10 my-3'>
+          <div className='flex flex-1 items-stretch text-center'>
+            <div
+                className={`${changeInPriceDetected && 'shadow-lg bg-red-400 text-white'} p-10 border drop-shadow rounded-md hover:shadow-lg transform transition-all duration-500`}>
+              <div>
+                <span className='text-2xl font-bold'>ETH = {lastPrice}$</span>
+              </div>
+              <p className='text-xl text-gray-500'>Current eth Price</p>
+            </div>
           </div>
-          <p className='text-xl text-gray-500'>Current eth Price</p>
+          <div className='flex flex-1 items-stretch text-center'>
+            <div className={`${changeInPriceDetected && "shadow-lg bg-red-400 text-white"} p-10 border drop-shadow rounded-md hover:shadow-lg transform transition-all duration-500`}>
+              <div>
+                <span className='text-2xl font-bold'>{(20 / ethCurrentPrice).toFixed(4)} Eth = 20 $</span>
+              </div>
+              <p className='text-xl text-gray-500'>Price per course</p>
+            </div>
+          </div>
         </div>
       </div>
-      <div className='flex flex-1 items-stretch text-center'>
-        <div className='p-10 border drop-shadow rounded-md'>
-          <div>
-            <span className='text-2xl font-bold'>0.004769 = 15$</span>
-          </div>
-          <p className='text-xl text-gray-500'>Price per course</p>
-        </div>
-      </div>
-    </div>
   );
 };
 
