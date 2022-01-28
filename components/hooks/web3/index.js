ea9d61f0111dd16c123431b063d6baad81f5aa23
Petrov Ilya <dnamix1@gmail.com>
2022-01-28 13:53:12 +0300

    enhanced hooks for network connectivity check /metamask is installed properly/ & account connectivity on metamask + combined hook providing wallet info

--- components/hooks/web3/index.js
+++ components/hooks/web3/index.js
@@ -1,15 +1,22 @@
-import {useHooks} from '../../providers/web3'
+import { useHooks } from "../../providers/web3";
 
 const enhanceHook = (swr) => {
   return {
     ...swr,
-    hasInitialResponse: swr.data || swr.error
-  }
-}
+    hasInitialResponse: swr.data || swr.error,
+  };
+};
 export const useAccount = () => {
-  return {account: enhanceHook(useHooks(hooks => hooks.useAccount)())};
-}
+  return { account: enhanceHook(useHooks((hooks) => hooks.useAccount)()) };
+};
 
 export const useNetwork = () => {
-  return {network: enhanceHook(useHooks(hooks => hooks.useNetwork)())};
-}
+  return { network: enhanceHook(useHooks((hooks) => hooks.useNetwork)()) };
+};
+
+export const useWalletInfo = () => {
+  const { account } = useAccount();
+  const { network } = useNetwork();
+  const allowPurchases = !!(account.data && network.isSupported);
+  return { account, network, allowPurchases };
+};
