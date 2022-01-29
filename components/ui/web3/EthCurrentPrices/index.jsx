5f900f36f369e0222f926a981b3c8b80ffbba2aa
Petrov Ilya <dnamix1@gmail.com>
2022-01-29 15:39:32 +0300

    dynamically-updating self-sufficient ethereum current market price board

--- components/ui/web3/EthCurrentPrices/index.jsx
+++ components/ui/web3/EthCurrentPrices/index.jsx
@@ -1,46 +1,80 @@
-import React, {useState, useEffect} from 'react';
-import {useEthPrice} from "@components/hooks/useEthPrice";
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import { useEthPrice } from "@components/hooks/useEthPrice";
 
 const EthCurrentPrices = () => {
-  const {data: ethCurrentPrice} = useEthPrice();
-  const [lastPrice, setLastPrice] = useState(null)
+  const { data: ethCurrentPrice } = useEthPrice();
+  const [lastPrice, setLastPrice] = useState(null);
+  const [animateEthIcon, setAnimateEthIcon] = useState(false);
   const [changeInPriceDetected, setChangeInPriceDetected] = useState(false);
 
   useEffect(() => {
     if (ethCurrentPrice !== lastPrice) {
-      setLastPrice(ethCurrentPrice)
-      setChangeInPriceDetected(true)
+      setChangeInPriceDetected(true);
       setTimeout(() => {
         setChangeInPriceDetected(false);
-      }, 600)
-    } else {
-      setLastPrice(ethCurrentPrice);
+      }, 600);
     }
-  }, [ethCurrentPrice])
+    setLastPrice(ethCurrentPrice);
+  }, [ethCurrentPrice, lastPrice]);
 
-  console.log({lastPrice, changeInPriceDetected})
   return (
-      <div className='max-w-3xl mx-auto flex justify-center'>
-        <div className='grid grid-cols-2 space-x-10 my-3'>
-          <div className='flex flex-1 items-stretch text-center'>
-            <div
-                className={`${changeInPriceDetected && 'shadow-lg bg-red-400 text-white'} p-10 border drop-shadow rounded-md hover:shadow-lg transform transition-all duration-500`}>
-              <div>
-                <span className='text-2xl font-bold'>ETH = {lastPrice}$</span>
-              </div>
-              <p className='text-xl text-gray-500'>Current eth Price</p>
+    <div className="max-w-3xl mx-auto flex justify-center">
+      <div className="grid grid-cols-2 space-x-10 my-3">
+        <div
+          className="flex flex-1 items-stretch text-center"
+          onMouseOver={() => setAnimateEthIcon(true)}
+          onMouseLeave={() => setAnimateEthIcon(false)}
+        >
+          <div
+            className={`${
+              changeInPriceDetected && "shadow-lg bg-red-400 text-white"
+            } p-10 border drop-shadow rounded-md hover:shadow-lg transform transition-all duration-500`}
+          >
+            <div className="flex items-center">
+              <span className="text-2xl font-bold">1</span>
+              <Image
+                src="/enthusia_logo.svg"
+                height="50"
+                width="50"
+                layout="fixed"
+                alt="ethereum"
+                className={`${animateEthIcon && "animate-pulse"}`}
+              />
+              <span className="text-2xl font-bold"> = {lastPrice}$</span>
             </div>
+            <p className="text-xl text-gray-500">Current eth Price</p>
           </div>
-          <div className='flex flex-1 items-stretch text-center'>
-            <div className={`${changeInPriceDetected && "shadow-lg bg-red-400 text-white"} p-10 border drop-shadow rounded-md hover:shadow-lg transform transition-all duration-500`}>
-              <div>
-                <span className='text-2xl font-bold'>{(20 / ethCurrentPrice).toFixed(4)} Eth = 20 $</span>
-              </div>
-              <p className='text-xl text-gray-500'>Price per course</p>
+        </div>
+        <div
+          className="flex flex-1 items-stretch text-center"
+          onMouseOver={() => setAnimateEthIcon(true)}
+          onMouseLeave={() => setAnimateEthIcon(false)}
+        >
+          <div
+            className={`${
+              changeInPriceDetected && "shadow-lg bg-red-400 text-white"
+            } p-10 border drop-shadow rounded-md hover:shadow-lg transform transition-all duration-500`}
+          >
+            <div className="flex items-center">
+              <span className="text-2xl font-bold">
+                {(20 / ethCurrentPrice).toFixed(6)}
+              </span>
+              <Image
+                src="/enthusia_logo.svg"
+                height="50"
+                width="50"
+                layout="fixed"
+                alt="ethereum"
+                className={`${animateEthIcon && "animate-pulse"}`}
+              />
+              <span className="text-2xl font-bold">= 20 $</span>
             </div>
+            <p className="text-xl text-gray-500">Price per course</p>
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
